ac6899e02b579a9bd34157bd13a963353b596024
xploter13 <xploter13@gmail.com>
2022-08-12 09:35:10 -0300

    inserção código RD Station

--- application/views/templates/styles.php
+++ application/views/templates/styles.php
@@ -17,3 +17,15 @@
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
     })(window,document,'script','dataLayer','GTM-NBJQFDL');</script>
     <!-- End Google Tag Manager -->    
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-2172900-50"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-2172900-50');
+    </script>
+    <!-- RD STATION -->
+    <script type="text/javascript" async src="https://d335luupugsy2.cloudfront.net/js/loader-scripts/7bbb9d58-235d-479a-9644-64ff9936e9f5-loader.js" ></script>
+    <!-- End RD STATION -->
